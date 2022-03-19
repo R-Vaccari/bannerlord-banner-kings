11787d2926c8def93119b38fdcf47d88164f1c6e
R. Vaccari <rodrigo.melo@penguinformula.com>
2022-03-19 17:22:58 +0000

    Legitimacy fix

--- BannerKings/Models/Populations/BKLegitimacyModel.cs
+++ BannerKings/Models/Populations/BKLegitimacyModel.cs
@@ -9,35 +9,16 @@ namespace BannerKings.Models
         {
             ExplainedNumber result = new ExplainedNumber();
             FeudalTitle title = BannerKingsConfig.Instance.TitleManager.GetTitle(settlement);
-            bool foreigner = settlement.Culture != settlement.Owner.Culture;
-            if (title.deJure == settlement.Owner)
-                result.Add((float)(foreigner ? LegitimacyType.Lawful_Foreigner : LegitimacyType.Lawful));
-            else result.Add((float)(foreigner ? LegitimacyType.Unlawful_Foreigner : LegitimacyType.Unlawful));
+
+            if (title != null)
+            {
+                bool foreigner = settlement.Culture != settlement.Owner.Culture;
+                if (title.deJure == settlement.Owner)
+                    result.Add((float)(foreigner ? LegitimacyType.Lawful_Foreigner : LegitimacyType.Lawful));
+                else result.Add((float)(foreigner ? LegitimacyType.Unlawful_Foreigner : LegitimacyType.Unlawful));
+            }
 
             return result;
-        }
-
-        public LegitimacyType GetRuleType(Settlement settlement, Hero hero)
-        {
-            FeudalTitle title = BannerKingsConfig.Instance.TitleManager.GetTitle(settlement);
-            bool foreigner = settlement.Culture != hero.Culture;
-            if (title.deJure == hero)
-                return foreigner ? LegitimacyType.Lawful_Foreigner : LegitimacyType.Lawful;
-            else return foreigner ? LegitimacyType.Unlawful_Foreigner : LegitimacyType.Unlawful;
-        }
-
-        public LegitimacyType GetRuleType(FeudalTitle title)
-        {
-            Hero hero = title.deFacto;
-            bool foreigner;
-            if (title.fief != null)
-                foreigner = title.fief.Culture != hero.Culture;
-            else if (title.deJure.MapFaction != null) foreigner = title.deJure.MapFaction.Culture != hero.Culture;
-            else foreigner = title.deJure.Culture != hero.Culture;
-
-            if (title.deJure == hero)
-                return foreigner ? LegitimacyType.Lawful_Foreigner : LegitimacyType.Lawful;
-            else return foreigner ? LegitimacyType.Unlawful_Foreigner : LegitimacyType.Unlawful;
         }
     }
 }
