2ff34703b23ffde05506cb7b46a93ac486cfe961
R. Vaccari <rodrigo.melo@penguinformula.com>
2022-05-25 13:19:49 +0100

    New foods

--- BannerKings/Managers/Items/BKItems.cs
+++ BannerKings/Managers/Items/BKItems.cs
@@ -6,11 +6,38 @@ namespace BannerKings.Managers.Items
 {
     public class BKItems : DefaultTypeInitializer<BKItems>
     {
-        private ItemObject bookHeartsDesire, bookSiege, bookStrategikon,
+        private ItemObject apple, orange, bread, pie, carrot, bookHeartsDesire, bookSiege, bookStrategikon,
             bookLeadership, bookTrade, bookDictionary, bookMounted;
         
         public override void Initialize()
         {
+            apple = Game.Current.ObjectManager.RegisterPresumedObject(new ItemObject("apple"));
+            InitializeTradeGood(apple,
+                new TextObject("{=!}Apples{@Plural}baskets of apples\\@}", null), "foods_basket_apple",
+                BKItemCategories.Instance.Apple, 5, 10f, ItemObject.ItemTypeEnum.Goods);
+
+            orange = Game.Current.ObjectManager.RegisterPresumedObject(new ItemObject("orange"));
+            InitializeTradeGood(orange,
+                new TextObject("{=!}Oranges{@Plural}baskets of oranges\\@}", null), "foods_orange_basket",
+                BKItemCategories.Instance.Apple, 5, 10f, ItemObject.ItemTypeEnum.Goods);
+
+            bread = Game.Current.ObjectManager.RegisterPresumedObject(new ItemObject("bread"));
+            InitializeTradeGood(bread,
+                new TextObject("{=!}Bread{@Plural}loathes of bread\\@}", null), "merchandise_bread",
+                BKItemCategories.Instance.Apple, 5, 10f, ItemObject.ItemTypeEnum.Goods);
+
+            pie = Game.Current.ObjectManager.RegisterPresumedObject(new ItemObject("pie"));
+            InitializeTradeGood(pie,
+                new TextObject("{=!}Pie{@Plural}baskets of pies\\@}", null), "kitchen_pie",
+                BKItemCategories.Instance.Apple, 5, 10f, ItemObject.ItemTypeEnum.Goods);
+
+            carrot = Game.Current.ObjectManager.RegisterPresumedObject(new ItemObject("carrot"));
+            InitializeTradeGood(carrot,
+                new TextObject("{=!}Carrots{@Plural}baskets of carrots\\@}", null), "foods_carrots_basket",
+                BKItemCategories.Instance.Apple, 5, 10f, ItemObject.ItemTypeEnum.Goods);
+
+
+
             bookHeartsDesire = Game.Current.ObjectManager.RegisterPresumedObject(new ItemObject("book_hearts_desire"));
             InitializeTradeGood(bookHeartsDesire, 
                 new TextObject("{=!}Heart's Desire{@Plural}collection of Heart's Desire books{\\@}", null), "lib_book_closed_a", 
