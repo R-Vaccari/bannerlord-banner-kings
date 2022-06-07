c915676d33b9770a8ede51289e524fb2bee98ea4
R. Vaccari <rodrigo.melo@penguinformula.com>
2022-06-07 18:22:29 +0100

    Fix

--- BannerKings/Managers/Institutions/Institution.cs
+++ BannerKings/Managers/Institutions/Institution.cs
@@ -1,5 +1,4 @@
-﻿
-namespace BannerKings.Managers.Institutions
+﻿namespace BannerKings.Managers.Institutions
 {
     public abstract class Institution
     {
@@ -9,41 +8,8 @@ namespace BannerKings.Managers.Institutions
         {
         }
 
-<<<<<<< HEAD
-        public Hero Leader
-        {
-            get
-            {
-                if (this.leader == null || !this.leader.IsAlive || this.leader.IsActive)
-                    this.leader = GenerateLeader();
-                return this.leader;
-            }
-        }
-
-        public abstract void Destroy();
-
-        public float Influence => this.influence;
-
-        public abstract Hero GenerateLeader();
-
-        public float GetFavor(Hero hero)
-        {
-            if (this.favors.ContainsKey(hero))
-                return this.favors[hero];
-            return 0f;
-        }
-
-        public void AddFavor(Hero hero, float favor)
-        {
-            if (this.favors.ContainsKey(hero))
-                this.favors[hero] += favor;
-            else this.favors.Add(hero, favor);
-        }
-
-=======
         public float Influence => influence;
 
         public abstract void Destroy();
->>>>>>> main
     }
 }
