197140df6d08756ad0e77b1d72e944192e900126
TheUnknown <seebruch.mirko@outlook.de>
2022-08-23 00:37:23 +0200

    Small refactoring.

--- BannerKings/Models/Vanilla/BKClanFinanceModel.cs
+++ BannerKings/Models/Vanilla/BKClanFinanceModel.cs
@@ -11,8 +11,7 @@ namespace BannerKings.Models.Vanilla
 {
     public class BKClanFinanceModel : DefaultClanFinanceModel
     {
-        public override ExplainedNumber CalculateClanGoldChange(Clan clan, bool includeDescriptions = false,
-            bool applyWithdrawals = false)
+        public override ExplainedNumber CalculateClanGoldChange(Clan clan, bool includeDescriptions = false, bool applyWithdrawals = false)
         {
             var baseResult = base.CalculateClanGoldChange(clan, true, applyWithdrawals);
             if (BannerKingsConfig.Instance.TitleManager != null)
@@ -24,8 +23,7 @@ namespace BannerKings.Models.Vanilla
             return baseResult;
         }
 
-        public override ExplainedNumber CalculateClanIncome(Clan clan, bool includeDescriptions = false,
-            bool applyWithdrawals = false)
+        public override ExplainedNumber CalculateClanIncome(Clan clan, bool includeDescriptions = false, bool applyWithdrawals = false)
         {
             var baseResult = base.CalculateClanIncome(clan, includeDescriptions, applyWithdrawals);
             if (BannerKingsConfig.Instance.TitleManager != null)
@@ -36,8 +34,7 @@ namespace BannerKings.Models.Vanilla
             return baseResult;
         }
 
-        public override ExplainedNumber CalculateClanExpenses(Clan clan, bool includeDescriptions = false,
-            bool applyWithdrawals = false)
+        public override ExplainedNumber CalculateClanExpenses(Clan clan, bool includeDescriptions = false, bool applyWithdrawals = false)
         {
             var baseResult = base.CalculateClanExpenses(clan, includeDescriptions, applyWithdrawals);
             if (BannerKingsConfig.Instance.TitleManager != null)
@@ -52,39 +49,34 @@ namespace BannerKings.Models.Vanilla
         {
             var kingdom = clan.Kingdom;
             var wkModel = (BKWorkshopModel) Campaign.Current.Models.WorkshopModel;
+
             foreach (var town in clan.Fiefs)
             {
                 foreach (var wk in town.Workshops)
                 {
                     if (wk.IsRunning && wk.Owner != clan.Leader && wk.WorkshopType.StringId != "artisans")
                     {
-                        result.Add(
-                            base.CalculateOwnerIncomeFromWorkshop(wk) *
-                            wkModel.CalculateWorkshopTax(town.Settlement, wk.Owner).ResultNumber,
+                        result.Add(base.CalculateOwnerIncomeFromWorkshop(wk) * wkModel.CalculateWorkshopTax(town.Settlement, wk.Owner).ResultNumber,
                             new TextObject("{=!}Taxes from {WORKSHOP} at {TOWN}")
                                 .SetTextVariable("WORKSHOP", wk.Name)
                                 .SetTextVariable("TOWN", town.Name));
                     }
                 }
 
-                if (BannerKingsConfig.Instance.AI.AcceptNotableAid(clan,
-                        BannerKingsConfig.Instance.PopulationManager.GetPopData(town.Settlement)))
+                if (!BannerKingsConfig.Instance.AI.AcceptNotableAid(clan, BannerKingsConfig.Instance.PopulationManager.GetPopData(town.Settlement)))
                 {
-                    foreach (var notable in town.Settlement.Notables)
+                    continue;
+                }
+
+                foreach (var notable in town.Settlement.Notables.Where(notable => notable.SupporterOf == clan && notable.Gold > 5000))
+                {
+                    result.Add(200f, new TextObject("{=!}Aid from {NOTABLE}").SetTextVariable("NOTABLE", notable.Name));
+                    if (applyWithdrawals)
                     {
-                        if (notable.SupporterOf == clan && notable.Gold > 5000)
-                        {
-                            result.Add(200f,
-                                new TextObject("{=!}Aid from {NOTABLE}").SetTextVariable("NOTABLE", notable.Name));
-                            if (applyWithdrawals)
-                            {
-                                notable.Gold -= 200;
-                            }
-                        }
+                        notable.Gold -= 200;
                     }
                 }
             }
-
 
             var dictionary = BannerKingsConfig.Instance.TitleManager.CalculateVassals(clan);
             if (dictionary.Count >= 0)
@@ -106,20 +98,21 @@ namespace BannerKings.Models.Vanilla
                 }
             }
 
-            if (!clan.IsUnderMercenaryService && clan.Kingdom != null &&
-                FactionManager.GetEnemyKingdoms(clan.Kingdom).Any())
+            if (!clan.IsUnderMercenaryService && clan.Kingdom != null && FactionManager.GetEnemyKingdoms(clan.Kingdom).Any())
             {
                 var title = BannerKingsConfig.Instance.TitleManager.GetSovereignTitle(kingdom);
                 if (title is {contract: { }} &&
                     title.contract.Rights.Contains(FeudalRights.Army_Compensation_Rights))
                 {
                     var model = new DefaultClanFinanceModel();
-                    var expense = model.GetType().GetMethod("AddExpenseFromLeaderParty",
-                        BindingFlags.Instance | BindingFlags.NonPublic);
+                    var expense = model.GetType().GetMethod("AddExpenseFromLeaderParty", BindingFlags.Instance | BindingFlags.NonPublic);
                     object[] array = {clan, new ExplainedNumber(), applyWithdrawals};
+
                     expense.Invoke(model, array);
+
                     var payment = ((ExplainedNumber) array[1]).ResultNumber * -1f;
                     var limit = clan.Tier * 1000f;
+
                     if (payment > limit)
                     {
                         payment = limit;
@@ -146,16 +139,16 @@ namespace BannerKings.Models.Vanilla
 
         private void AddMercenaryIncome(Clan clan, ref ExplainedNumber goldChange, bool applyWithdrawals)
         {
-            if (clan.IsUnderMercenaryService && clan.Leader != null && clan.Kingdom != null)
+            if (!clan.IsUnderMercenaryService || clan.Leader == null || clan.Kingdom == null)
             {
-                var num = MathF.Ceiling(clan.Influence *
-                                        (1f / Campaign.Current.Models.ClanFinanceModel.RevenueSmoothenFraction())) *
-                          clan.MercenaryAwardMultiplier;
-                var education = BannerKingsConfig.Instance.EducationManager.GetHeroEducation(clan.Leader);
-                if (education.Lifestyle == DefaultLifestyles.Instance.Mercenary)
-                {
-                    goldChange.Add((int) (num * 0.15f), new TextObject("{=!}Lifestyle"));
-                }
+                return;
+            }
+
+            var num = MathF.Ceiling(clan.Influence * (1f / Campaign.Current.Models.ClanFinanceModel.RevenueSmoothenFraction())) * clan.MercenaryAwardMultiplier;
+            var education = BannerKingsConfig.Instance.EducationManager.GetHeroEducation(clan.Leader);
+            if (education.Lifestyle == DefaultLifestyles.Instance.Mercenary)
+            {
+                goldChange.Add((int) (num * 0.15f), new TextObject("{=!}Lifestyle"));
             }
         }
 
@@ -164,14 +157,16 @@ namespace BannerKings.Models.Vanilla
             var wkModel = (BKWorkshopModel) Campaign.Current.Models.WorkshopModel;
             foreach (var wk in clan.Leader.OwnedWorkshops)
             {
-                if (wk.IsRunning && wk.Settlement.OwnerClan != clan)
+                if (!wk.IsRunning || wk.Settlement.OwnerClan == clan)
                 {
-                    var tax = wkModel.CalculateWorkshopTax(wk.Settlement, clan.Leader).ResultNumber;
-                    result.Add(base.CalculateOwnerIncomeFromWorkshop(wk) * -tax,
-                        new TextObject("{=!}{WORKSHOP} taxes to {CLAN}")
-                            .SetTextVariable("WORKSHOP", wk.Name)
-                            .SetTextVariable("CLAN", wk.Settlement.OwnerClan.Name));
+                    continue;
                 }
+
+                var tax = wkModel.CalculateWorkshopTax(wk.Settlement, clan.Leader).ResultNumber;
+                result.Add(base.CalculateOwnerIncomeFromWorkshop(wk) * -tax,
+                    new TextObject("{=!}{WORKSHOP} taxes to {CLAN}")
+                        .SetTextVariable("WORKSHOP", wk.Name)
+                        .SetTextVariable("CLAN", wk.Settlement.OwnerClan.Name));
             }
 
             var data = BannerKingsConfig.Instance.CourtManager.GetCouncil(clan);
@@ -206,15 +201,10 @@ namespace BannerKings.Models.Vanilla
                 return;
             }
 
-            var amount = 0f;
             var dictionary = BannerKingsConfig.Instance.TitleManager.CalculateVassals(suzerain.deJure.Clan, clan);
-            foreach (var title in dictionary[clan])
-            {
-                amount += (int) title.dueTax;
-            }
+            var amount = dictionary[clan].Aggregate(0f, (current, title) => current + (int) title.dueTax);
 
-            result.Add(-amount,
-                new TextObject("{=!}Taxes to {SUZERAIN}").SetTextVariable("SUZERAIN", suzerain.deJure.Name));
+            result.Add(-amount, new TextObject("{=!}Taxes to {SUZERAIN}").SetTextVariable("SUZERAIN", suzerain.deJure.Name));
         }
     }
 }
