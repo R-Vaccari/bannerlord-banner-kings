805b96af03d7b0219f5069fcdf537bd8da5a7c5a
TheUnknown <seebruch.mirko@outlook.de>
2022-08-22 16:29:46 +0200

    Added property for failed reasons.

--- BannerKings/Managers/Goals/Goal.cs
+++ BannerKings/Managers/Goals/Goal.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Localization;
 
@@ -11,6 +12,15 @@ namespace BannerKings.Managers.Goals
         internal Goal(string stringId, GoalUpdateType goalUpdateType) : base(stringId)
         {
             this.goalUpdateType = goalUpdateType;
+        }
+
+        internal List<TextObject> FailedReasons
+        {
+            get
+            {
+                IsFulfilled(out var failedReasons);
+                return failedReasons;
+            }
         }
 
         internal abstract bool IsAvailable();
