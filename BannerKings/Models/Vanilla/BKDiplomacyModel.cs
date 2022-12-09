e3e4ab19ffa01363ad8dd594b5ce86174f2a3299
R. Vaccari <rodrigo.melo@penguinformula.com>
2022-12-09 19:49:28 +0000

    Clan wealth as factor for war declaration

--- BannerKings/Models/Vanilla/BKDiplomacyModel.cs
+++ BannerKings/Models/Vanilla/BKDiplomacyModel.cs
@@ -1,10 +1,34 @@
-﻿namespace BannerKings.Models.Vanilla
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.Localization;
+
+namespace BannerKings.Models.Vanilla
 {
+    public class BKDiplomacyModel : DefaultDiplomacyModel
+    {
+        public override float GetScoreOfDeclaringWar(IFaction factionDeclaresWar, IFaction factionDeclaredWar, IFaction evaluatingClan, out TextObject warReason)
+        {
+            float result = base.GetScoreOfDeclaringWar(factionDeclaresWar, factionDeclaredWar, evaluatingClan, out warReason);
+
+            if (factionDeclaresWar.IsKingdomFaction)
+            {
+                Kingdom kingdom = (Kingdom)factionDeclaresWar;
+                float wealthFactor = 0;
+                foreach (var clan in kingdom.Clans)
+                {
+                    if (!clan.IsUnderMercenaryService)
+                    {
+                        wealthFactor += (clan.Gold - 25000) / 100000f;
+                    }
+                }
+
+                result *= wealthFactor / kingdom.Clans.Count;
+            }
+
+            return result;
+        }
+
+    }
 }
 
 
-/*class BKDiplomacyModel : DefaultDiplomacyModel
-{
-
-
-}*/
