33a576d095d889ddf7f627f73a696f3758bca568
R. Vaccari <rodrigo.melo@penguinformula.com>
2022-12-12 16:57:58 +0000

    Interest groups

---
+++ BannerKings/Behaviours/Diplomacy/Groups/InterestGroup.cs
@@ -0,0 +1,25 @@
+﻿using BannerKings.Managers.Titles.Laws;
+using System.Collections.Generic;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.CharacterDevelopment;
+
+namespace BannerKings.Behaviours.Diplomacy.Groups
+{
+    public class InterestGroup : BannerKingsObject
+    {
+        public InterestGroup(string stringId) : base(stringId)
+        {
+        }
+
+        public TraitObject MainTrait { get; private set; }
+        public Hero Leader { get; private set; }
+        public List<Hero> Members { get; private set; }
+        public bool DemandsCouncil { get; private set; }
+        public bool AllowsCommoners { get; private set; }
+        public bool AllowsNobles { get; private set; }
+        public List<PolicyObject> SupportedPolicies { get; private set; }
+        public List<PolicyObject> ShunnedPolicies { get; private set; }
+        public List<DemesneLaw> SupportedLaws { get; private set; }
+        public List<DemesneLaw> ShunnedLaws { get; private set; }
+    }
+}
--- BannerKings/Managers/Traits/BKTraits.cs
+++ BannerKings/Managers/Traits/BKTraits.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using TaleWorlds.CampaignSystem.CharacterDevelopment;
 using TaleWorlds.Core;
 using TaleWorlds.Localization;
@@ -14,7 +13,18 @@ namespace BannerKings.Managers.Traits
         public TraitObject Forgiving { get; private set;  }
         public TraitObject Arrogant { get; private set; }
         public TraitObject Patient { get; private set; }
-        public override IEnumerable<TraitObject> All => throw new NotImplementedException();
+        public override IEnumerable<TraitObject> All
+        {
+            get
+            {
+                yield return Zealous;
+                yield return Conservative;
+                yield return Ambitious;
+                yield return Forgiving;
+                yield return Arrogant;
+                yield return Patient;
+            }
+        }
 
         public override void Initialize()
         {
@@ -23,28 +33,28 @@ namespace BannerKings.Managers.Traits
                 new TextObject("{=!}Zealotry represents one's religious fervour. Zealous people will care to follow their faith's teachings and support it's expansion."), 
                 false, -2, 2);
 
-            Zealous = Game.Current.ObjectManager.RegisterPresumedObject(new TraitObject("Zealot"));
-            Zealous.Initialize(new TextObject("{=}Zealous"),
+            Conservative = Game.Current.ObjectManager.RegisterPresumedObject(new TraitObject("Conservative"));
+            Conservative.Initialize(new TextObject("{=}Conservative"),
                 new TextObject("{=!}Zealotry represents one's religious fervour. Zealous people will care to follow their faith's teachings and support it's expansion."),
                 false, -2, 2);
 
-            Zealous = Game.Current.ObjectManager.RegisterPresumedObject(new TraitObject("Zealot"));
-            Zealous.Initialize(new TextObject("{=}Zealous"),
+            Ambitious = Game.Current.ObjectManager.RegisterPresumedObject(new TraitObject("Ambitious"));
+            Ambitious.Initialize(new TextObject("{=}Ambitious"),
                 new TextObject("{=!}Zealotry represents one's religious fervour. Zealous people will care to follow their faith's teachings and support it's expansion."),
                 false, -2, 2);
 
-            Zealous = Game.Current.ObjectManager.RegisterPresumedObject(new TraitObject("Zealot"));
-            Zealous.Initialize(new TextObject("{=}Zealous"),
+            Forgiving = Game.Current.ObjectManager.RegisterPresumedObject(new TraitObject("Forgiving"));
+            Forgiving.Initialize(new TextObject("{=}Forgiving"),
                 new TextObject("{=!}Zealotry represents one's religious fervour. Zealous people will care to follow their faith's teachings and support it's expansion."),
                 false, -2, 2);
 
-            Zealous = Game.Current.ObjectManager.RegisterPresumedObject(new TraitObject("Zealot"));
-            Zealous.Initialize(new TextObject("{=}Zealous"),
+            Arrogant = Game.Current.ObjectManager.RegisterPresumedObject(new TraitObject("Arrogant"));
+            Arrogant.Initialize(new TextObject("{=}Arrogant"),
                 new TextObject("{=!}Zealotry represents one's religious fervour. Zealous people will care to follow their faith's teachings and support it's expansion."),
                 false, -2, 2);
 
-            Zealous = Game.Current.ObjectManager.RegisterPresumedObject(new TraitObject("Zealot"));
-            Zealous.Initialize(new TextObject("{=}Zealous"),
+            Patient = Game.Current.ObjectManager.RegisterPresumedObject(new TraitObject("Patient"));
+            Patient.Initialize(new TextObject("{=}Patient"),
                 new TextObject("{=!}Zealotry represents one's religious fervour. Zealous people will care to follow their faith's teachings and support it's expansion."),
                 false, -2, 2);
         }
