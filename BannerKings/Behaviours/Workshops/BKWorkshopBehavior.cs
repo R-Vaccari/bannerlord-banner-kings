9e3176cf4c09b927ac623002895c493f70f474c0
R. Vaccari <rodrigo.melo@penguinformula.com>
2022-12-15 15:01:11 +0000

    Workshop dialogue TBD

--- BannerKings/Behaviours/Workshops/BKWorkshopBehavior.cs
+++ BannerKings/Behaviours/Workshops/BKWorkshopBehavior.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Conversation;
 using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.CampaignSystem.Settlements.Workshops;
 
@@ -23,6 +23,7 @@ namespace BannerKings.Behaviours.Workshops
         public override void RegisterEvents()
         {
             CampaignEvents.DailyTickTownEvent.AddNonSerializedListener(this, OnTownDailyTick);
+            CampaignEvents.OnSessionLaunchedEvent.AddNonSerializedListener(this, OnSessionLaunched);
         }
 
         public override void SyncData(IDataStore dataStore)
@@ -33,6 +34,19 @@ namespace BannerKings.Behaviours.Workshops
             {
                 inventories = new Dictionary<Workshop, WorkshopData>();
             }
+        }
+
+        private void OnSessionLaunched(CampaignGameStarter starter)
+        {
+            /*starter.AddPlayerLine("workshop_notable_owner_begin_2", 
+                "hero_main_options", 
+                "workshop_owner_notable_multiple_response", 
+                "{=LuLttpc5}I wish to buy one of your workshops.", 
+                new ConversationSentence.OnConditionDelegate(this.workshop_notable_owner_begin_multiple_on_condition), 
+                new ConversationSentence.OnConsequenceDelegate(this.workshop_notable_owner_answer_list_workshops_on_condition), 
+                100, 
+                new ConversationSentence.OnClickableConditionDelegate(this.player_war_status_clickable_condition), 
+                null);*/
         }
 
         /*private void OnGameLoaded(CampaignGameStarter starter)
