155778f4404c62c48eb93ef963e67069d75fda29
R. Vaccari <rodrigo.melo@penguinformula.com>
2022-12-15 14:19:05 +0000

    Error fix

--- BannerKings/Behaviours/BKSettlementBehavior.cs
+++ BannerKings/Behaviours/BKSettlementBehavior.cs
@@ -375,9 +375,16 @@ namespace BannerKings.Behaviours
                     var items = new HashSet<ItemObject>();
                     if (town.Villages.Count > 0)
                     {
-                        foreach (var tuple in town.Villages.Select(vil => BannerKingsConfig.Instance.PopulationManager.GetPopData(vil.Settlement)).SelectMany(data => BannerKingsConfig.Instance.PopulationManager.GetProductions(data)))
+                        foreach (var vil in town.Villages)
                         {
-                            items.Add(tuple.Item1);
+                            var villagePopData = BannerKingsConfig.Instance.PopulationManager.GetPopData(vil.Settlement);
+                            if (villagePopData != null && villagePopData.VillageData != null)
+                            {
+                                foreach (var tuple in BannerKingsConfig.Instance.PopulationManager.GetProductions(villagePopData))
+                                {
+                                    items.Add(tuple.Item1);
+                                }
+                            }
                         }
                     }
 
