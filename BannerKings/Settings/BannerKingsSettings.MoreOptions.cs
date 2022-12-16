90f78f2df172a545d09164556b705ac057849a72
Brandon Mathis <brandon@darkspyre.org>
2022-12-16 15:14:54 -0600

    ok

--- BannerKings/Settings/BannerKingsSettings.MoreOptions.cs
+++ BannerKings/Settings/BannerKingsSettings.MoreOptions.cs
@@ -16,10 +16,10 @@ namespace BannerKings.Settings
             HintText = "Gold Cost per hour.  50 = default",
             RequireRestart = false,
             Order = 1)]        
-        public float SmithingGoldCostPerHour { get; set; } = 50f;
+        public float SmithingGoldCostPerHour { get; set; } = 10f;
 
         [SettingPropertyGroup("Smithing")]
         [SettingPropertyFloatingInteger("Hour per modifier", 0f, 5f, HintText = "Modifiers how long it takes to craft per stamina used. Lower is faster. Default 1", Order = 2)]
-        public float SmithingStaminaPerHourModifier { get; set; } = 1f;
+        public float SmithingStaminaPerHourModifier { get; set; } = 0.25f;
     }
 }
