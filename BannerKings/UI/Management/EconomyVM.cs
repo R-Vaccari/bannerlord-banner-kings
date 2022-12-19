07059636d7a0b7b97360bf563119e2d6097ca86e
R. Vaccari <rodrigo.melo@penguinformula.com>
2022-12-19 10:32:34 +0000

    Refactored tournament spin up

--- BannerKings/UI/Management/EconomyVM.cs
+++ BannerKings/UI/Management/EconomyVM.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using BannerKings.Managers.Policies;
@@ -6,6 +7,7 @@ using BannerKings.UI.Items;
 using BannerKings.UI.Items.UI;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Inventory;
+using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.CampaignSystem.Roster;
 using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.Core;
@@ -392,9 +394,36 @@ namespace BannerKings.UI.Management
         {
             var tournament = new TournamentData(settlement.Town);
             data.TournamentData = tournament;
-            roster = tournament.Roster;
-            InventoryManager.OpenScreenAsStash(tournament.Roster);
-            RefreshValues();
+
+            var list = new List<InquiryElement>();
+            foreach (var element in MobileParty.MainParty.ItemRoster)
+            {
+                var item = element.EquipmentElement.Item;
+                if (item.HasWeaponComponent || item.HasArmorComponent || (item.HasHorseComponent && !item.HorseComponent.IsLiveStock))
+                {
+                    list.Add(new InquiryElement(item, 
+                        element.EquipmentElement.GetModifiedItemName().ToString(),
+                        new ImageIdentifier(item),
+                        true,
+                        ""));
+                }
+            }
+
+            MBInformationManager.ShowMultiSelectionInquiry(new MultiSelectionInquiryData(
+                new TextObject().ToString(),
+                new TextObject().ToString(),
+                null,
+                true,
+                1,
+                GameTexts.FindText("str_accept").ToString(),
+                GameTexts.FindText("str_reject").ToString(),
+                delegate (List<InquiryElement> list)
+                {
+                    ItemObject item = (ItemObject)list[0].Identifier;
+                    tournament.SetPrize(item);
+                    RefreshValues();
+                },
+                null));
         }
 
         public override void OnFinalize()
