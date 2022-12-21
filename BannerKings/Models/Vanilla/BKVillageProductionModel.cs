1d8425f24a4aae250a7d9e139c1f8fc6f56b48af
R. Vaccari <rodrigo.melo@penguinformula.com>
2022-12-21 16:23:56 +0000

    Fixed animal productions

--- BannerKings/Models/Vanilla/BKVillageProductionModel.cs
+++ BannerKings/Models/Vanilla/BKVillageProductionModel.cs
@@ -223,7 +223,7 @@ namespace BannerKings.Models.Vanilla
             if (valid)
             {
                 var acres = data.Pastureland;
-                result.Add((acres * data.GetAcreOutput("pasture")) / Math.Min(item.HorseComponent.MeatCount, 0));
+                result.Add((acres * data.GetAcreOutput("pasture")) / Math.Max(item.HorseComponent.MeatCount, 1));
                 if (item.IsMountable)
                 {
                     result.AddFactor(-0.5f);
