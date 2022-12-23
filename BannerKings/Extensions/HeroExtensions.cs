592713e46597870bf89eb18e5bb9f7ba2958c7a0
R. Vaccari <rodrigo.melo@penguinformula.com>
2022-12-23 15:00:41 +0000

    Hero extension

---
+++ BannerKings/Extensions/HeroExtensions.cs
@@ -0,0 +1,16 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Localization;
+
+namespace BannerKings.Extensions
+{
+    public static class HeroExtensions
+    {
+        public static void SetCulturalName(this Hero hero)
+        {
+            TextObject firstName;
+            TextObject fullName;
+            NameGenerator.Current.GenerateHeroNameAndHeroFullName(hero, out firstName, out fullName, false);
+            hero.SetName(fullName, firstName);
+        }
+    }
+}
