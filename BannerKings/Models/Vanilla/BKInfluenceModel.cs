1778e02757457296da8c418aaa3351b749503e49
R. Vaccari <rodrigo.melo@penguinformula.com>
2022-12-26 15:42:58 +0000

    Fix vassal limit effect

--- BannerKings/Models/Vanilla/BKInfluenceModel.cs
+++ BannerKings/Models/Vanilla/BKInfluenceModel.cs
@@ -149,7 +149,7 @@ namespace BannerKings.Models.Vanilla
             if (currentVassals > vassalLimit)
             {
                 float factor = vassalLimit - currentVassals;
-                baseResult.Add(-factor * 3f, new TextObject("{=!}Over Vassal Limit"));
+                baseResult.Add(factor * 1.5f, new TextObject("{=!}Over Vassal Limit"));
             }
 
             if (i > 0)
