259eddfcf534951e7390c396631e2996c650e4f6
R. Vaccari <rodrigo.melo@penguinformula.com>
2022-12-29 19:13:08 +0000

    HeroExtensions

--- BannerKings/Extensions/HeroExtensions.cs
+++ BannerKings/Extensions/HeroExtensions.cs
@@ -1,16 +1,25 @@
 ﻿using TaleWorlds.CampaignSystem;
-using TaleWorlds.Localization;
 
-namespace BannerKings.Extensions
+namespace BannerKings.Utils.Extensions
 {
     public static class HeroExtensions
     {
-        public static void SetCulturalName(this Hero hero)
+        public static bool IsPlayer(this Hero hero)
         {
-            TextObject firstName;
-            TextObject fullName;
-            NameGenerator.Current.GenerateHeroNameAndHeroFullName(hero, out firstName, out fullName, false);
-            hero.SetName(fullName, firstName);
+            return hero.StringId == Hero.MainHero.StringId;
         }
+
+        public static bool IsClanLeader(this Hero hero)
+        {
+            return hero.Clan?.Leader == hero;
+        }
+
+        public static bool IsKingdomLeader(this Hero hero)
+        {
+            return hero.Clan?.Kingdom?.Leader == hero;
+        }
+
+        public static bool IsCommonBorn(this Hero hero) => hero.CharacterObject != null && hero.CharacterObject.OriginalCharacter != null
+            && hero.CharacterObject.OriginalCharacter.IsTemplate;
     }
 }
