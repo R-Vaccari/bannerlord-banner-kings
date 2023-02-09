1c136025d81b7c803e7c36b7d14f657e4e03a6f9
R. Vaccari <rodrigo.melo@penguinformula.com>
2023-02-09 17:01:35 +0000

    Versioning

--- BannerKings/BannerKingsConfig.cs
+++ BannerKings/BannerKingsConfig.cs
@@ -27,7 +27,7 @@ namespace BannerKings
     public class BannerKingsConfig
     {
         public const string VersionNumber = "1.2.6.5";
-        public const string VersionEdition = "Standard";
+        public const string VersionEdition = "CEK";
         public string VersionName => VersionNumber + VersionEdition;
 
         private List<ITypeInitializer> modInitializers = new List<ITypeInitializer>();
