fc2b5c9de6ba2d8cdb875096c6f37a2a5a63646f
R. Vaccari <rodrigo.melo@penguinformula.com>
2023-03-09 09:26:29 +0000

    New election setup

--- BannerKings/Managers/Kingdoms/Succession/BKKingElectionDecision.cs
+++ BannerKings/Managers/Kingdoms/Succession/BKKingElectionDecision.cs
@@ -1,12 +1,20 @@
-﻿using TaleWorlds.CampaignSystem;
+﻿using BannerKings.Managers.Titles;
+using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Election;
+using TaleWorlds.SaveSystem;
 
-namespace BannerKings.Managers.Kingdoms
+namespace BannerKings.Managers.Kingdoms.Succession
 {
     public class BKKingElectionDecision : KingSelectionKingdomDecision
     {
-        public BKKingElectionDecision(Clan proposerClan, Clan clanToExclude = null) : base(proposerClan, clanToExclude)
+        [SaveableProperty(100)] protected FeudalTitle Title { get; set; }
+        [SaveableProperty(101)] protected Hero PreviousRuler { get; set; }
+
+        public BKKingElectionDecision(Clan proposerClan, Clan clanToExclude = null, FeudalTitle title = null, Hero previousRuler = null) 
+            : base(proposerClan, clanToExclude)
         {
+            Title = title;
+            PreviousRuler = previousRuler;
         }
 
         public override void ApplyChosenOutcome(DecisionOutcome chosenOutcome)
@@ -16,7 +24,7 @@ namespace BannerKings.Managers.Kingdoms
             if (title != null)
             {
                 var deJure = title.deJure;
-                var king = ((KingSelectionDecisionOutcome) chosenOutcome).King;
+                var king = ((KingSelectionDecisionOutcome)chosenOutcome).King;
                 if (deJure != king)
                 {
                     BannerKingsConfig.Instance.TitleManager.InheritTitle(deJure, king, title);
---
+++ BannerKings/Managers/Kingdoms/Succession/FeudalElectiveDecision.cs
@@ -0,0 +1,30 @@
+﻿using BannerKings.Managers.Titles;
+using System.Collections.Generic;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Election;
+
+namespace BannerKings.Managers.Kingdoms.Succession
+{
+    public class FeudalElectiveDecision : BKKingElectionDecision
+    {
+        public FeudalElectiveDecision(Clan proposerClan, FeudalTitle title) : base(proposerClan, null, title)
+        {
+        }
+
+        public override bool IsAllowed()
+        {
+            return BannerKingsConfig.Instance.TitleModel.GetSuccessionCandidates(PreviousRuler, Title.contract).Count > 1;
+        }
+
+        public override IEnumerable<DecisionOutcome> DetermineInitialCandidates()
+        {
+            BannerKingsConfig.Instance.TitleModel.GetSuccessionCandidates(PreviousRuler, Title.contract);
+            return base.DetermineInitialCandidates();
+        }
+
+        public override float CalculateMeritOfOutcome(DecisionOutcome candidateOutcome)
+        {
+            return base.CalculateMeritOfOutcome(candidateOutcome);
+        }
+    }
+}
