bb2fbd650b8663007a9d52e544affdd0f29df848
R. Vaccari <rodrigo.melo@penguinformula.com>
2023-04-23 10:58:58 +0100

    Book focus adjustment

--- BannerKings/Managers/Education/Books/BookType.cs
+++ BannerKings/Managers/Education/Books/BookType.cs
@@ -45,10 +45,7 @@ namespace BannerKings.Managers.Education.Books
                 hero.AddSkillXp(Skill, 300f);
                 if (hero.HeroDeveloper.GetFocus(Skill) < Campaign.Current.Models.CharacterDevelopmentModel.MaxFocusPerSkill)
                 {
-                    if (hero.HeroDeveloper.UnspentFocusPoints > 0)
-                    {
-                        hero.HeroDeveloper.UnspentFocusPoints++;
-                    }
+                    hero.HeroDeveloper.UnspentFocusPoints++;
                     hero.HeroDeveloper.AddFocus(Skill, 1);
                 }
                 else
