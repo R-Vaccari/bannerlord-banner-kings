370fc9e0206daeed8df5fe09d5f7d1ee2aa4874c
R. Vaccari <rodrigo.melo@penguinformula.com>
2023-07-11 20:35:01 +0100

    Cour buy and consume goods

--- BannerKings/Managers/Court/Grace/CourtGrace.cs
+++ BannerKings/Managers/Court/Grace/CourtGrace.cs
@@ -1,10 +1,13 @@
-﻿using BannerKings.UI.Court;
+﻿using BannerKings.Actions;
+using BannerKings.UI.Court;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using TaleWorlds.CampaignSystem;
-using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.CampaignSystem.Roster;
 using TaleWorlds.Core;
 using TaleWorlds.Library;
+using TaleWorlds.Localization;
 using static BannerKings.Managers.Court.Grace.CourtExpense;
 
 namespace BannerKings.Managers.Court.Grace
@@ -51,6 +54,89 @@ namespace BannerKings.Managers.Court.Grace
         public void Update()
         {
             grace += GraceChange;
+            if (CampaignTime.Now.GetDayOfSeason != 1 || data.Clan.IsUnderMercenaryService) return;
+
+            Dictionary<ItemCategory, int> toBuy = new Dictionary<ItemCategory, int>();
+            Dictionary<ItemCategory, int> toConsume = new Dictionary<ItemCategory, int>();
+            foreach (CourtExpense expense in Expenses)
+            {
+                foreach (var pair in expense.ItemCategories)
+                {
+                    if (toBuy.ContainsKey(pair.Key))
+                    {
+                        toBuy[pair.Key] += pair.Value;
+                    }
+                    else
+                    {
+                        toBuy.Add(pair.Key, pair.Value);
+                    }
+
+                    if (expense.ConsumeItems)
+                    {
+                        if (toConsume.ContainsKey(pair.Key))
+                        {
+                            toConsume[pair.Key] += pair.Value;
+                        }
+                        else
+                        {
+                            toConsume.Add(pair.Key, pair.Value);
+                        }
+                    }
+                }
+            }
+
+            if (data.Location != null)
+            {
+                TextObject reason = null;
+                if (data.Clan == Clan.PlayerClan) 
+                    reason = new TextObject("{=!}Your court has spent {GOLD}{GOLD_ICON} buying {ITEMS} items for its good requirements.");
+                BuyGoodsAction.BuyBestToWorst(data.Location.Settlement.Stash,
+                    data.Location,
+                    data.Clan.Leader,
+                    toBuy,
+                    data.Clan.Leader.Gold,
+                    reason);
+            }
+
+            float graceChange = 0f;
+            int totalItems = 0;
+            int totalConsumed = 0;
+            foreach (var pair in toConsume)
+            {
+                int consumed = 0;
+                totalItems += pair.Value;
+                graceChange -= 2f * pair.Value;
+                if (data.Location != null)
+                {
+                    ItemRoster roster = data.Location.Settlement.Stash;
+                    foreach (ItemRosterElement element in roster)
+                    {
+                        if (consumed >= pair.Value) break;
+
+                        if (element.EquipmentElement.Item.ItemCategory == pair.Key)
+                        {
+                            int max = MathF.Min(pair.Value - consumed, element.Amount);
+                            roster.AddToCounts(element.EquipmentElement, -max);
+                            consumed += max;
+                            float priceModifier = element.EquipmentElement.ItemModifier != null ?
+                                element.EquipmentElement.ItemModifier.PriceMultiplier : 1f;
+                            graceChange += max * 2f * priceModifier;
+                            totalConsumed += max;
+                        }
+                    }
+                }
+            }
+
+            if (data.Clan == Clan.PlayerClan)
+            {
+                InformationManager.DisplayMessage(new InformationMessage(
+                    new TextObject("{=!}Your court has consumed {COUNT} out of {DESIRED} required goods, changing your grace by {GRACE}.")
+                    .SetTextVariable("GRACE", graceChange.ToString("0.0"))
+                    .SetTextVariable("COUNT", totalConsumed)
+                    .SetTextVariable("DESIRED", totalItems)
+                    .ToString(),
+                    Color.FromUint(graceChange >= 0f ? Utils.TextHelper.COLOR_LIGHT_BLUE : Utils.TextHelper.COLOR_LIGHT_RED)));
+            }
         }
 
         public void AddExpense(Clan clan, CourtExpense expense)
@@ -73,7 +159,7 @@ namespace BannerKings.Managers.Court.Grace
             if (diff > 0f)
             {
                 float income = BannerKingsConfig.Instance.ClanFinanceModel.CalculateClanIncome(clan).ResultNumber;
-                return MBRandom.RoundRandomized(income * diff * 10f);
+                return MBRandom.RoundRandomized(income * diff * 15f);
             }
 
             return 0;
