d9e1a972d598550f3ab57c1bde5a71713618d626
R. Vaccari <rodrigo.melo@penguinformula.com>
2023-07-11 20:35:15 +0100

    Schemes first classes

---
+++ BannerKings/Managers/Schemes/Scheme.cs
@@ -0,0 +1,25 @@
+﻿using TaleWorlds.CampaignSystem;
+
+namespace BannerKings.Managers.Schemes
+{
+    public class Scheme : BannerKingsObject
+    {
+        public Scheme(string stringId) : base(stringId)
+        {
+        }
+
+        public bool IsSecret { get; private set; }
+        public Hero Agent { get; private set; }
+
+        public void PostInitialize()
+        {
+
+        }
+
+        public enum SchemeType
+        {
+            Diplomatic,
+            Roguery
+        }
+    }
+}
