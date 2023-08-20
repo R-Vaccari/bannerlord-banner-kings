5772e67ce34bb0be86e121de71d65fd4be1f35ce
R. Vaccari <rodrigo.melo@penguinformula.com>
2023-08-20 18:52:33 +0100

    War decision based on Casus Belli

---
+++ BannerKings/Behaviours/Diplomacy/Barterable/BKWarBarterable.cs
@@ -0,0 +1,35 @@
+﻿using BannerKings.Behaviours.Diplomacy.Wars;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.BarterSystem.Barterables;
+using TaleWorlds.Localization;
+
+namespace BannerKings.Behaviours.Diplomacy.Barterable
+{
+    public class BKWarBarterable : DeclareWarBarterable
+    {
+        public BKWarBarterable(CasusBelli justification, IFaction declaringFaction, IFaction otherFaction) : base(declaringFaction, otherFaction)
+        {
+        }
+
+        public CasusBelli CasusBelli { get; private set; }
+
+        public override int GetUnitValueForFaction(IFaction faction)
+        {
+            int result = 0;
+            Clan evaluatingFaction = ((faction is Clan) ? ((Clan)faction) : ((Kingdom)faction).RulingClan);
+            TextObject reason;
+            if (faction.MapFaction == base.OriginalOwner.MapFaction)
+            {
+                result = (int)BannerKingsConfig.Instance.DiplomacyModel.GetScoreOfDeclaringWar(base.OriginalOwner.MapFaction, 
+                    OtherFaction.MapFaction, evaluatingFaction, out reason, CasusBelli).ResultNumber;
+            }
+            else if (faction.MapFaction == OtherFaction.MapFaction)
+            {
+                result = (int)BannerKingsConfig.Instance.DiplomacyModel.GetScoreOfDeclaringWar(OtherFaction.MapFaction, 
+                    base.OriginalOwner.MapFaction, evaluatingFaction, out reason, CasusBelli).ResultNumber;
+            }
+
+            return result;
+        }
+    }
+}
--- BannerKings/Behaviours/Diplomacy/Wars/BKDeclareWarDecision.cs
+++ BannerKings/Behaviours/Diplomacy/Wars/BKDeclareWarDecision.cs
@@ -1,4 +1,6 @@
-﻿using TaleWorlds.CampaignSystem;
+﻿using BannerKings.Behaviours.Diplomacy.Barterable;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.CharacterDevelopment;
 using TaleWorlds.CampaignSystem.Election;
 
 namespace BannerKings.Behaviours.Diplomacy.Wars
@@ -9,6 +11,16 @@ namespace BannerKings.Behaviours.Diplomacy.Wars
         public BKDeclareWarDecision(CasusBelli casusBelli, Clan proposerClan, IFaction factionToDeclareWarOn) : base(proposerClan, factionToDeclareWarOn)
         {
             CasusBelli = casusBelli;
+        }
+
+        public override float DetermineSupport(Clan clan, DecisionOutcome possibleOutcome)
+        {
+            _ = clan.Leader;
+            DeclareWarDecisionOutcome obj = (DeclareWarDecisionOutcome)possibleOutcome;
+            float num = new BKWarBarterable(CasusBelli, Kingdom, FactionToDeclareWarOn).GetValueForFaction(clan) *
+                Campaign.Current.Models.DiplomacyModel.DenarsToInfluence();
+
+            return obj.ShouldWarBeDeclared ? num : -num;
         }
 
         public override void ApplyChosenOutcome(DecisionOutcome chosenOutcome)
