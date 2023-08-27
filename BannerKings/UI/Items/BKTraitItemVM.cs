24afe57209603917f7b3068b16ef726934db5b9e
R. Vaccari <rodrigo.melo@penguinformula.com>
2023-08-27 14:53:56 +0100

    Religion popup updates

--- BannerKings/UI/Items/BKTraitItemVM.cs
+++ BannerKings/UI/Items/BKTraitItemVM.cs
@@ -1,6 +1,8 @@
 ﻿using TaleWorlds.CampaignSystem.CharacterDevelopment;
+using TaleWorlds.Core;
 using TaleWorlds.Core.ViewModelCollection.Information;
 using TaleWorlds.Library;
+using TaleWorlds.Localization;
 
 namespace BannerKings.UI.Items
 {
@@ -8,7 +10,7 @@ namespace BannerKings.UI.Items
     {
         private readonly TraitObject _traitObj;
         private HintViewModel _hint;
-        private string _traitId;
+        private string _traitId, descriptionText;
         private int _value;
 
         public BKTraitItemVM(TraitObject traitObj, bool positive)
@@ -17,9 +19,27 @@ namespace BannerKings.UI.Items
             TraitId = traitObj.StringId;
             Value = positive ? 2 : -2;
             Hint = new HintViewModel(traitObj.Description);
+            descriptionText = new TextObject("{=!}Being {TRAIT} is considered virtuous")
+                .SetTextVariable("TRAIT", GameTexts.FindText("str_trait_name_" + traitObj.StringId.ToLower(),
+                        (Value + MathF.Abs(traitObj.MinValue)).ToString()))
+                .ToString();
         }
 
         [DataSourceProperty] public string Name => _traitObj.Name.ToString();
+
+        [DataSourceProperty]
+        public string DescriptionText
+        {
+            get => descriptionText;
+            set
+            {
+                if (value != descriptionText)
+                {
+                    descriptionText = value;
+                    OnPropertyChangedWithValue(value);
+                }
+            }
+        }
 
         [DataSourceProperty]
         public string TraitId
