c211de1c6c38c84b072755883aaa3352ffc9a871
R. Vaccari <rodrigo.melo@penguinformula.com>
2023-09-03 17:35:44 +0100

    Ignore aspects for now

---
+++ BannerKings/Managers/Titles/Governments/ContractDuty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TaleWorlds.CampaignSystem;
+
+namespace BannerKings.Managers.Titles.Governments
+{
+    public class ContractDuty : ContractAspect
+    {
+        private Action<Hero, Hero> execute;
+        public ContractDuty(string stringId) : base(stringId)
+        {
+        }
+
+        public override void PostInitialize()
+        {
+
+        }
+
+        public int YearlyLimit { get; private set; }
+        public void Execute(Hero suzerain, Hero vassal) => execute(suzerain, vassal);
+    }
+}
---
+++ BannerKings/Managers/Titles/Governments/ContractRight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BannerKings.Managers.Titles.Governments
+{
+    public class ContractRight : ContractAspect
+    {
+        public ContractRight(string stringId) : base(stringId)
+        {
+        }
+
+        public override void PostInitialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
