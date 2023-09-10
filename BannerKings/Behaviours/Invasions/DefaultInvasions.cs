c94db4542731967a157cb469d8225ef904160fec
R. Vaccari <rodrigo.melo@penguinformula.com>
2023-09-10 16:00:09 +0100

    Invasion basic setup

---
+++ BannerKings/Behaviours/Invasions/DefaultInvasions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace BannerKings.Behaviors.Invasions
+{
+    public class DefaultInvasions : DefaultTypeInitializer<DefaultInvasions, Invasion>
+    {
+        public override IEnumerable<Invasion> All
+        {
+            get
+            {
+                foreach (Invasion item in ModAdditions)
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public override void Initialize()
+        {
+        }
+    }
+}
