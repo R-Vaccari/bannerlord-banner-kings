7722338fdc5a82676039b355306ebeb3e784f89b
R. Vaccari <rodrigo.melo@penguinformula.com>
2023-09-17 13:20:32 +0100

    Copied vanilla for debugging

--- BannerKings/Models/Vanilla/BKPartyConsumptionModel.cs
+++ BannerKings/Models/Vanilla/BKPartyConsumptionModel.cs
@@ -1,9 +1,13 @@
 ﻿using BannerKings.Components;
 using BannerKings.Managers.Education.Lifestyles;
 using BannerKings.Managers.Skills;
+using Helpers;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.CharacterDevelopment;
 using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.CampaignSystem.MapEvents;
 using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Siege;
 using TaleWorlds.Core;
 
 namespace BannerKings.Models.Vanilla
@@ -29,9 +33,65 @@ namespace BannerKings.Models.Vanilla
             return base.DoesPartyConsumeFood(mobileParty);
         }
 
+        private void VanillaCalculatePerkEffects(MobileParty party, ref ExplainedNumber result)
+        {
+            int num = 0;
+            for (int i = 0; i < party.MemberRoster.Count; i++)
+            {
+                if (party.MemberRoster.GetCharacterAtIndex(i).Culture.IsBandit)
+                {
+                    num += party.MemberRoster.GetElementNumber(i);
+                }
+            }
+            for (int j = 0; j < party.PrisonRoster.Count; j++)
+            {
+                if (party.PrisonRoster.GetCharacterAtIndex(j).Culture.IsBandit)
+                {
+                    num += party.PrisonRoster.GetElementNumber(j);
+                }
+            }
+            if (party.LeaderHero != null && party.LeaderHero.GetPerkValue(DefaultPerks.Roguery.Promises) && num > 0)
+            {
+                float value = (float)num / (float)this.NumberOfMenOnMapToEatOneFood * DefaultPerks.Roguery.Promises.PrimaryBonus;
+                result.Add(value, DefaultPerks.Roguery.Promises.Name, null);
+            }
+            PerkHelper.AddPerkBonusForParty(DefaultPerks.Athletics.Spartan, party, false, ref result);
+            PerkHelper.AddPerkBonusForParty(DefaultPerks.Steward.WarriorsDiet, party, true, ref result);
+            if (party.EffectiveQuartermaster != null)
+            {
+                PerkHelper.AddEpicPerkBonusForCharacter(DefaultPerks.Steward.PriceOfLoyalty, party.EffectiveQuartermaster.CharacterObject, DefaultSkills.Steward, true, ref result, 250);
+            }
+            TerrainType faceTerrainType = Campaign.Current.MapSceneWrapper.GetFaceTerrainType(party.CurrentNavigationFace);
+            if (faceTerrainType == TerrainType.Forest || faceTerrainType == TerrainType.Steppe)
+            {
+                PerkHelper.AddPerkBonusForParty(DefaultPerks.Scouting.Foragers, party, true, ref result);
+            }
+            if (party.IsGarrison && party.CurrentSettlement != null && party.CurrentSettlement.Town.IsUnderSiege)
+            {
+                PerkHelper.AddPerkBonusForTown(DefaultPerks.Athletics.StrongLegs, party.CurrentSettlement.Town, ref result);
+            }
+            if (party.Army != null)
+            {
+                PerkHelper.AddPerkBonusForParty(DefaultPerks.Steward.StiffUpperLip, party, true, ref result);
+            }
+            SiegeEvent siegeEvent = party.SiegeEvent;
+            if (((siegeEvent != null) ? siegeEvent.BesiegerCamp : null) != null)
+            {
+                if (party.SiegeEvent.BesiegerCamp.HasInvolvedPartyForEventType(party.Party, MapEvent.BattleTypes.Siege) && party.HasPerk(DefaultPerks.Steward.SoundReserves, true))
+                {
+                    PerkHelper.AddPerkBonusForParty(DefaultPerks.Steward.SoundReserves, party, false, ref result);
+                }
+                if (party.HasPerk(DefaultPerks.Steward.MasterOfPlanning, false))
+                {
+                    result.AddFactor(DefaultPerks.Steward.MasterOfPlanning.PrimaryBonus, DefaultPerks.Steward.MasterOfPlanning.Name);
+                }
+            }
+        }
+
         public override ExplainedNumber CalculateDailyFoodConsumptionf(MobileParty party, ExplainedNumber baseConsumption)
         {
-            var result = base.CalculateDailyFoodConsumptionf(party, baseConsumption);
+            VanillaCalculatePerkEffects(party, ref baseConsumption);
+            baseConsumption.LimitMax(0f);
             var leader = party.LeaderHero;
 
             if (leader != null)
@@ -45,7 +105,7 @@ namespace BannerKings.Models.Vanilla
                     var cow = Game.Current.ObjectManager.GetObject<ItemObject>("cow");
                     int cattleHeads = party.ItemRoster.GetItemNumber(cow);
 
-                    result.Add(cattleHeads * 0.06f, BKPerks.Instance.KheshigRaider.Name);
+                    baseConsumption.Add(cattleHeads * 0.06f, BKPerks.Instance.KheshigRaider.Name);
                 }
 
                 if (party.Army != null && party.SiegeEvent != null)
@@ -54,19 +114,18 @@ namespace BannerKings.Models.Vanilla
                     var armyEducation = BannerKingsConfig.Instance.EducationManager.GetHeroEducation(armyLeader);
                     if (armyEducation.HasPerk(BKPerks.Instance.SiegeOverseer))
                     {
-                        result.AddFactor(-0.15f, BKPerks.Instance.SiegeOverseer.Name);
+                        baseConsumption.AddFactor(-0.15f, BKPerks.Instance.SiegeOverseer.Name);
                     }
 
                     if (faceTerrainType == TerrainType.Desert && armyEducation.Lifestyle != null &&
                         armyEducation.Lifestyle.Equals(DefaultLifestyles.Instance.Jawwal))
                     {
-                        result.AddFactor(-0.3f, DefaultLifestyles.Instance.Jawwal.Name);
+                        baseConsumption.AddFactor(-0.3f, DefaultLifestyles.Instance.Jawwal.Name);
                     }
                 }
             }
 
-
-            return result;
+            return baseConsumption;
         }
     }
 }
