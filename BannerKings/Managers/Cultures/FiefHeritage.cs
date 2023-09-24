37befb4da73abf64039dbe329b0e6c59522ae415
R. Vaccari <rodrigo.melo@penguinformula.com>
2023-09-24 09:56:00 +0100

    Fief heritage initial impl

---
+++ BannerKings/Managers/Cultures/FiefHeritage.cs
@@ -0,0 +1,15 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Settlements;
+
+namespace BannerKings.Managers.Cultures
+{
+    public class FiefHeritage : BannerKingsObject
+    {
+        public FiefHeritage(string stringId) : base(stringId)
+        {
+        }
+
+        public Settlement Settlement { get; private set; }
+        public CultureObject Culture { get; private set; }
+    }
+}
