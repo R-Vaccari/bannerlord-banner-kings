e41b38cdff81e0d5a8440b60a17948ef77f7799e
rodrigo <rodrigo.v.melo@hotmail.com>
2023-11-04 18:46:51 +0000

    Get allies extension

---
+++ BannerKings/Extensions/FactionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using TaleWorlds.CampaignSystem;
+
+namespace BannerKings.Extensions
+{
+    public static class FactionExtensions
+    {
+        public static List<IFaction> GetAllies(this IFaction faction)
+        {
+            List<IFaction> list = new List<IFaction>(3);
+            foreach (var stance in faction.Stances)
+            {
+                if (stance.IsAllied)
+                {
+                    list.Add(stance.Faction1 == faction ? stance.Faction2 : stance.Faction1);
+                }
+            }
+
+            return list;
+        }
+    }
+}
