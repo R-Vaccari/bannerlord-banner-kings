06443f8d289aaff678a841e8154326088f3a6053
rodrigo <rodrigo.v.melo@hotmail.com>
2024-01-11 16:44:20 +0000

    Clan tier fix

--- BannerKings/Models/Vanilla/BKClanTierModel.cs
+++ BannerKings/Models/Vanilla/BKClanTierModel.cs
@@ -16,6 +16,34 @@ namespace BannerKings.Models.Vanilla
             return result;
         }
 
+        public override int GetPartyLimitForTier(Clan clan, int clanTierToCheck)
+        {
+            var result = base.GetPartyLimitForTier(clan, clanTierToCheck);
+            if (BannerKingsConfig.Instance.TitleManager != null && BannerKingsConfig.Instance.CourtManager != null)
+            {
+                var title = BannerKingsConfig.Instance.TitleManager.GetHighestTitle(clan.Leader);
+                if (title != null)
+                {
+                    result += 5 - (int)title.TitleType;
+                }
+            }
+
+            return result;
+        }
+
+        public override int CalculateTier(Clan clan)
+        {
+            int result = MinClanTier;
+            for (int i = MinClanTier + 1; i <= MaxClanTier; i++)
+            {
+                if (clan.Renown >= GetRequiredRenownForTier(i))
+                {
+                    result = i;
+                }
+            }
+            return result;
+        }
+
         public override int GetRequiredRenownForTier(int tier) => (int) (base.GetRequiredRenownForTier(tier) * BannerKingsSettings.Instance.ClanRenown);      
     }
 }
