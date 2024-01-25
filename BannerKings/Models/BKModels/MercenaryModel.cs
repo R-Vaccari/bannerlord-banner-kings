4b5835fb7e0e359b58e9c8d8be59cd436e680853
rodrigo <rodrigo.v.melo@hotmail.com>
2024-01-25 20:19:04 +0000

    Mercenary equipment crash fix, fix for certain cultures missing items, item availability adjustments

--- BannerKings/Models/BKModels/MercenaryModel.cs
+++ BannerKings/Models/BKModels/MercenaryModel.cs
@@ -36,11 +36,15 @@ namespace BannerKings.Models.BKModels
         {
             yield return DefaultItemCategories.MediumArmor;
             yield return DefaultItemCategories.LightArmor;
+            yield return DefaultItemCategories.Garment;
             yield return DefaultItemCategories.MeleeWeapons3;
             yield return DefaultItemCategories.RangedWeapons3;
+            yield return DefaultItemCategories.MeleeWeapons4;
+            yield return DefaultItemCategories.RangedWeapons4;
             yield return DefaultItemCategories.Horse;
             yield return DefaultItemCategories.WarHorse;
             yield return DefaultItemCategories.Shield3;
+            yield return DefaultItemCategories.Shield4;
             yield return DefaultItemCategories.HorseEquipment2;
             yield return DefaultItemCategories.HorseEquipment3;
             yield return DefaultItemCategories.HorseEquipment4;
@@ -50,39 +54,23 @@ namespace BannerKings.Models.BKModels
         public override bool IsEquipmentAdequate(ItemObject item, CharacterObject troop, bool levy)
         {
             MarketGroup group = DefaultMarketGroups.Instance.GetMarket(troop.Culture);
-            if (item.IsCraftedByPlayer)
-            {
-                return false;
-            }
+            if (item.IsCraftedByPlayer) return false;
 
-            if (!GetNonCulturalCategories().Contains(item.ItemCategory))
-            {
-                if (group != null && group.GetSpawn((CultureObject)item.Culture) <= 0f)
-                {
-                    return false;
-                }
-                else if (item.Culture != troop.Culture)
-                {
-                    return false;
-                }
-            }
+            if (item.Culture == null) return true;
 
-            if (levy)
-            {
-                if (GetLevyCategories().Contains(item.ItemCategory))
-                {
-                    return true;
-                }
-            }
+            bool culture = false;
+            bool adequate = false;
+            if (GetNonCulturalCategories().Contains(item.ItemCategory)) culture = true; 
             else
             {
-                if (GetProCategories().Contains(item.ItemCategory)) 
-                {
-                    return true;
-                }
+                if (item.Culture == troop.Culture) culture = true;
+                else if (group != null && group.GetSpawn((CultureObject)item.Culture) > 0f) culture = true; 
             }
+            
+            if (levy && GetLevyCategories().Contains(item.ItemCategory)) adequate = true;
+            else if (!levy && GetProCategories().Contains(item.ItemCategory)) adequate = true;
 
-            return false;
+            return adequate && culture;
         }
     }
 }
