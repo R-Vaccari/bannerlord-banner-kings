c0356db53ad9ffb18a7239032aa52273c8b8d0ad
rodrigo <rodrigo.v.melo@hotmail.com>
2024-04-14 11:14:56 +0100

    Crash fix

--- BannerKings/Managers/Innovations/Eras/BKTroopAdvancement.cs
+++ BannerKings/Managers/Innovations/Eras/BKTroopAdvancement.cs
@@ -20,17 +20,20 @@ namespace BannerKings.Managers.Innovations.Eras
 
         internal void SetEquipment()
         {
-            MBEquipmentRoster equipmentRoster = (MBEquipmentRoster)Character.GetType().BaseType
-                           .GetField("_equipmentRoster", BindingFlags.Instance | BindingFlags.NonPublic)
-                           .GetValue(Character);
-            List<Equipment> rosterEquipment = (List<Equipment>)equipmentRoster.GetType().GetField("_equipments", BindingFlags.Instance | BindingFlags.NonPublic)
-                .GetValue(equipmentRoster);
-            rosterEquipment.Clear();
+            if (UpgradeEquipment != null)
+            {
+                MBEquipmentRoster equipmentRoster = (MBEquipmentRoster)Character.GetType().BaseType
+                                           .GetField("_equipmentRoster", BindingFlags.Instance | BindingFlags.NonPublic)
+                                           .GetValue(Character);
+                List<Equipment> rosterEquipment = (List<Equipment>)equipmentRoster.GetType().GetField("_equipments", BindingFlags.Instance | BindingFlags.NonPublic)
+                    .GetValue(equipmentRoster);
+                rosterEquipment.Clear();
 
-            List<Equipment> equipmentsToAdd = (List<Equipment>)equipmentRoster.GetType().GetField("_equipments", BindingFlags.Instance | BindingFlags.NonPublic)
-               .GetValue(UpgradeEquipment);
-            foreach (Equipment equipment in equipmentsToAdd)
-                rosterEquipment.Add(equipment);
+                List<Equipment> equipmentsToAdd = (List<Equipment>)equipmentRoster.GetType().GetField("_equipments", BindingFlags.Instance | BindingFlags.NonPublic)
+                   .GetValue(UpgradeEquipment);
+                foreach (Equipment equipment in equipmentsToAdd)
+                    rosterEquipment.Add(equipment);
+            }
         }
     }
 }
