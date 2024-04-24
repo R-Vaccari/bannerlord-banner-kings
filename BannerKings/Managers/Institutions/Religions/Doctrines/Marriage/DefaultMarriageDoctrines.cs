a50824f083311aed55d04b1a63d7cba0efa21a99
rodrigo <rodrigo.v.melo@hotmail.com>
2024-04-24 08:38:49 +0100

    Marriage doctrines

---
+++ BannerKings/Managers/Institutions/Religions/Doctrines/Marriage/DefaultMarriageDoctrines.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using TaleWorlds.Localization;
+
+namespace BannerKings.Managers.Institutions.Religions.Doctrines.Marriage
+{
+    public class DefaultMarriageDoctrines : DefaultTypeInitializer<DefaultMarriageDoctrines, MarriageDoctrine>
+    {
+        public MarriageDoctrine Monogamy { get; set; }
+        public MarriageDoctrine AvunculateMonogamy { get; set; }
+        public MarriageDoctrine Concubinage { get; set; }
+        public MarriageDoctrine OpenConcubinage { get; set; }
+        public MarriageDoctrine AvunculateConcubinage { get; set; }
+        public MarriageDoctrine Polygamy { get; set; }
+        public MarriageDoctrine AvunculatePolygamy { get; set; }
+        public override IEnumerable<MarriageDoctrine> All
+        {
+            get
+            {
+                yield return Monogamy;
+                yield return Concubinage;
+                yield return Polygamy;
+                yield return OpenConcubinage;
+                yield return AvunculateConcubinage;
+                yield return AvunculatePolygamy;
+                yield return AvunculateMonogamy;
+            }
+        }
+
+        public override void Initialize()
+        {
+            Monogamy = new MarriageDoctrine("Monogamy",
+                new TextObject("{=!}Monogamy"),
+                new TextObject("{=!}Monogamy is a marriage system in which two spouses, male and female, are equally bound to each other. The marriage is always diplomatically binding. It does not accept any close degree of blood relation."),
+                new TextObject(),
+                new List<Doctrine>(),
+                0,
+                2,
+                true);
+
+            AvunculateMonogamy = new MarriageDoctrine("AvunculateMonogamy",
+                new TextObject("{=!}Monogamy"),
+                new TextObject("{=!}Avunculate Monogamy is a marriage system in which two spouses, male and female, are equally bound to each other. The marriage is always diplomatically binding. Spouses may not be close relatives."),
+                new TextObject(),
+                new List<Doctrine>(),
+                0,
+                1,
+                true);
+
+            AvunculateConcubinage = new MarriageDoctrine("AvunculateConcubinage",
+                new TextObject("{=!}Avunculate Concubinage"),
+                new TextObject("{=!}Avunculate Concubinage doctrine allows for clan leaders to take up to 3 concubines/consorts. Concubines may be forced into concubinage when imprisoned. Concubines yield no diplomatic bindings such as alliances. Spouses may not be close relatives."),
+                new TextObject(),
+                new List<Doctrine>(),
+                3,
+                1,
+                true,
+                true);
+
+            OpenConcubinage = new MarriageDoctrine("OpenConcubinage",
+                new TextObject("{=!}Open Concubinage"),
+                new TextObject("{=!}Open Concubinage doctrine allows for clan leaders to take up to 3 concubines/consorts. Concubines may be forced into concubinage when imprisoned. Concubines yield no diplomatic bindings such as alliances. There are no restrictions towards consanguinity: close family may marry."),
+                new TextObject(),
+                new List<Doctrine>(),
+                2,
+                0,
+                true,
+                true);
+
+            Concubinage = new MarriageDoctrine("Concubinage",
+                new TextObject("{=!}Concubinage"),
+                new TextObject("{=!}Concubinage doctrine allows for clan leaders to take up to 3 concubines/consorts. Concubines may be forced into concubinage when imprisoned. Concubines yield no diplomatic bindings such as alliances. It does not accept any close degree of blood relation."),
+                new TextObject(),
+                new List<Doctrine>(),
+                3,
+                2,
+                true,
+                true);
+
+            Polygamy = new MarriageDoctrine("Polygamy",
+                new TextObject("{=!}Polygamy"),
+                new TextObject("{=!}Polygamy is a marriage system in which a lead spouse (a clan leader) may have one primary spouse, as weall as 3 secondary spouses. Secondary spouses may not be forced into marriage, and their marriages are also diplomatically binding, alongside the primary spouse. It does not accept any close degree of blood relation."),
+                new TextObject(),
+                new List<Doctrine>(),
+                3,
+                2,
+                true);
+
+            AvunculatePolygamy = new MarriageDoctrine("AvunculatePolygamy",
+                new TextObject("{=!}Polygamy"),
+                new TextObject("{=!}Avunculate Polygamy is a marriage system in which a lead spouse (a clan leader) may have one primary spouse, as weall as 3 secondary spouses. Secondary spouses may not be forced into marriage, and their marriages are also diplomatically binding, alongside the primary spouse. "),
+                new TextObject(),
+                new List<Doctrine>(),
+                3,
+                1,
+                true);
+        }
+    }
+}
--- BannerKings/Managers/Institutions/Religions/Doctrines/Marriage/MarriageDoctrine.cs
+++ BannerKings/Managers/Institutions/Religions/Doctrines/Marriage/MarriageDoctrine.cs
@@ -6,9 +6,53 @@ namespace BannerKings.Managers.Institutions.Religions.Doctrines.Marriage
     public class MarriageDoctrine : Doctrine
     {
         public MarriageDoctrine(string id, TextObject name, TextObject description, TextObject effects, 
-            List<Doctrine> incompatibleDoctrines, int concubines, bool homosexual) : 
+            List<Doctrine> incompatibleDoctrines, 
+            int concubines, 
+            int consanguinity,
+            bool acceptsUntolerated,
+            bool isConcubinage = false) : 
             base(id, name, description, effects, incompatibleDoctrines, false)
         {
+            Consorts = concubines;
+            Consanguinity = consanguinity;
+            AcceptsUntolerated = acceptsUntolerated;
+            IsConcubinage = isConcubinage;
+        }
+
+        public int Consorts { get; private set; }
+        public int Consanguinity { get; private set; }
+        public bool AcceptsUntolerated { get; private set; }
+        public bool IsConcubinage { get; private set; }
+
+        public TextObject ConsortsExplanation
+        {
+            get
+            {
+                if (Consorts == 0) return new TextObject("{=!}Additional spouses or concubines/consorts are not tolerated by the faith.");
+                else if (IsConcubinage) return new TextObject("{=!}Up to {COUNT} concubines/consorts are allowed in addition to your primary spouse. Concubines can be forced into concubinage and do not yield diplomatic bindings.")
+                    .SetTextVariable("COUNT", Consorts);
+                return new TextObject("{=!}Up to {COUNT} secondary spouses are allowed in addition to your primary spouse. Spouses may not be forcefully married and allow for diplomatic bindings such as alliances.")
+                    .SetTextVariable("COUNT", Consorts);
+            }
+        }
+
+        public TextObject ConsanguinityExplanation
+        {
+            get
+            {
+                if (Consanguinity == 0) return new TextObject("{=!}Spouses are allowed to have any level of consanguinity.");
+                return new TextObject("{=!}Spouses are not allowed to have any blood relatives up to {COUNT} generation(s).")
+                    .SetTextVariable("COUNT", Consanguinity);
+            }
+        }
+
+        public TextObject UntoleratedExplanation
+        {
+            get
+            {
+                if (AcceptsUntolerated) return new TextObject("{=!}Spouses may be of faith groups considered untolerated, but not hostile.");
+                return new TextObject("{=!}Spouse faiths must be of the same faith group, but not necessarily the same faith. Untolerated or hostile faiths are disallowed.");
+            }
         }
     }
 }
