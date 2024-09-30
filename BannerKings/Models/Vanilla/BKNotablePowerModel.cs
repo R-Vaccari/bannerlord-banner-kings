5aea174a684ccd0fc56c83e277072393b54b0f4d
R. Vaccari <rodrigo.melo@penguinformula.com>
2024-09-30 20:16:50 +0100

    Power limit & preacher power

--- BannerKings/Models/Vanilla/BKNotablePowerModel.cs
+++ BannerKings/Models/Vanilla/BKNotablePowerModel.cs
@@ -1,4 +1,6 @@
-﻿using TaleWorlds.CampaignSystem;
+﻿using BannerKings.Extensions;
+using BannerKings.Managers.Populations;
+using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.GameComponents;
 
 namespace BannerKings.Models.Vanilla
@@ -7,11 +9,17 @@ namespace BannerKings.Models.Vanilla
     {
         public override ExplainedNumber CalculateDailyPowerChangeForHero(Hero hero, bool includeDescriptions = false)
         {
-            var result = base.CalculateDailyPowerChangeForHero(hero, includeDescriptions);
-            if (hero.CurrentSettlement is {Town: { }} &&
-                hero.GovernorOf == hero.CurrentSettlement.Town)
-            {
+            ExplainedNumber result = base.CalculateDailyPowerChangeForHero(hero, includeDescriptions);
+            if (hero.CurrentSettlement != null && hero.CurrentSettlement.Town != null && hero.GovernorOf == hero.CurrentSettlement.Town)
                 result.Add(0.3f);
+
+            if (1000f < hero.Power) result.Add((hero.Power / 1000f) * -3f);
+            if (hero.IsPreacher && hero.OwnedWorkshops.Count == 0)
+            {
+                result.Add(0.1f);
+                PopulationData data = hero.CurrentSettlement.PopulationData();
+                if (data.ReligionData != null && data.ReligionData.DominantReligion == BannerKingsConfig.Instance.ReligionsManager.GetHeroReligion(hero))
+                    result.Add(0.1f);
             }
 
             return result;
