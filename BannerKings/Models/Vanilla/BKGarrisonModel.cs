a2f9d97e3950f8f8f3da96787f815df07da74e01
R. Vaccari <rodrigo.melo@penguinformula.com>
2025-01-24 10:52:42 +0000

    Error correction

--- BannerKings/Models/Vanilla/BKGarrisonModel.cs
+++ BannerKings/Models/Vanilla/BKGarrisonModel.cs
@@ -118,7 +118,7 @@ namespace BannerKings.Models.Vanilla
                     }
 
                     float num10 = (settlement.Town.IsOwnerUnassigned ? 0.75f : 0.5f);
-                    if (settlement.OwnerClan == mobileParty.LeaderHero.Clan || settlement.OwnerClan == mobileParty.Party.Owner.MapFaction.Leader.Clan)
+                    if (mobileParty != null && settlement.OwnerClan != null && settlement.OwnerClan == mobileParty.MapFaction.Leader.Clan)
                     {
                         num10 = 1f;
                     }
